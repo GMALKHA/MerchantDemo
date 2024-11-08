36292eb1d184dc6e20a0f3d685291023288c19f9
Georgi Malkhasyan (CW) <gmalkhasyan@paypal.com>
2024-11-07 16:43:05 -0800

    Initial Commit

---
+++ MerchantDemo/MerchantDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerchantDemoApp.swift
+//  MerchantDemo
+//
+//  Created by Georgi Malkhasyan on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MerchantDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
